dab343224ed132dc9f01b584a56924f03d1a01bc
shoshi-20 <shos61926@gmail.com>
2023-10-08 02:32:17 +0300

    fix build errors3

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
 import { connectToDB } from "@/lib/mongoose";
+import Product from '@/lib/medels/product.model'
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
-import Product from "@/lib/medels/product.model";
 
-//export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
